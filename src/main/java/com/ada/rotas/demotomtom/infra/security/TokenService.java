bd6fe354f9dde9cf2eb03362c763f399b1c76e02
Igor Ribeiro <igor.ribeiro2334@gmail.com>
2024-10-25 19:20:57 -0300

    chore: update auth0

--- src/main/java/com/ada/rotas/demotomtom/infra/security/TokenService.java
+++ src/main/java/com/ada/rotas/demotomtom/infra/security/TokenService.java
@@ -1,10 +1,10 @@
 package com.ada.rotas.demotomtom.infra.security;
 
+import com.ada.rotas.demotomtom.model.usuario.Usuario;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.ada.rotas.demotomtom.model.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
